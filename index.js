3f263957799463be0fc212fbfdb4f4e1d90b4145
Tuhin Mukherjee <ifeelme07@gmail.com>
2025-08-26 14:09:41 +0530

    modified index.js

--- index.js
+++ index.js
@@ -268,6 +268,6 @@ app.use((err, req, res, next) => {
 });
 
 // 404 handler
-app.use('*', (req, res) => {
-  res.status(404).json({ message: 'Route not found' });
+app.use(/.*/, (req, res) => {
+  res.status(404).json({ message: "Route not found" });
 });
